25dbc35299f91460de70b19098c7693e90847ad0
RAPHAEL DE LEMOS PIRES <raphaellpires@gmail.com>
2026-05-29 08:52:28 -0300

    Obtendo post por id

---
+++ src/main/java/com/phaeljf/workshopmongo/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.phaeljf.workshopmongo.resources;
+
+
+import com.phaeljf.workshopmongo.domain.Post;
+import com.phaeljf.workshopmongo.domain.User;
+import com.phaeljf.workshopmongo.dto.UserDTO;
+import com.phaeljf.workshopmongo.repository.PostRepository;
+import com.phaeljf.workshopmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService postService;
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+}
---
+++ src/main/java/com/phaeljf/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.phaeljf.workshopmongo.services;
+
+import com.phaeljf.workshopmongo.domain.Post;
+import com.phaeljf.workshopmongo.repository.PostRepository;
+import com.phaeljf.workshopmongo.repository.UserRepository;
+import com.phaeljf.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto Não encontrado! Id: " + id));
+    }
+
+
+}
